735a98dad75d61f82d39c4931c38f86b2b37549f
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-01 16:17:32 +0700

    Change object type to Shape and Add reg3 square object

--- com/solid/lsp/Main.java
+++ com/solid/lsp/Main.java
@@ -4,18 +4,24 @@ public class Main {
     private static final int height = 5;
 
     public static void main(String[] args) {
-        Rectangle reg1 = new Rectangle(width, height);
+        Shape reg1 = new Rectangle(width, height);
         System.out.println("==== Rectangle ====");
         System.out.println("Width = " + width);
         System.out.println("Height = " + height);
         System.out.println("Area = " + reg1.getArea());
 
-        Rectangle reg2 = new Square(5);
+        Shape reg2 = new Square(5);
         reg2.setWidth(width);
         reg2.setHeight(height);
         System.out.println("====   Square  ====");
         System.out.println("Width = " + width);
         System.out.println("Height = " + height);
         System.out.println("Area = " + reg2.getArea());
+
+        Shape reg3 = new Square(10);
+        System.out.println("====  Square ====");
+        System.out.println("Width = " + reg3.getWidth());
+        System.out.println("Height = " + reg3.getHeight());
+        System.out.println("Area  = " + reg3.getArea());
     }
 }
