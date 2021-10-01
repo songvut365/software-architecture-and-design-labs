de9e1d535a7df78e028ee74966619d270f5414d2
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-01 18:18:49 +0700

    Change Book to FilePrinter

--- com/solid/book/BookPublisher.java
+++ com/solid/book/BookPublisher.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BookPublisher {
     public static void main(String[] args) {
-        Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        FilePrinter book = new FilePrinter("Tyland", List.of("I", "moved", "here", "recently", "too"));
         book.printToFile();
     }
 }
