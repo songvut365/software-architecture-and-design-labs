2dd6985227d9a14316a406a05e69e3364a530627
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-01 16:19:10 +0700

    Remove override in method get/set

--- com/solid/lsp/Square.java
+++ com/solid/lsp/Square.java
@@ -1,16 +1,6 @@
-public class Square extends Rectangle {
-
+public class Square extends Shape {
     Square(int side) {
         this.setSide(side);
-    }
-
-    @Override
-    public void setWidth(int width) {this.setSide(width);
-    }
-
-    @Override
-    public void setHeight(int height) {
-        this.setSide(height);
     }
 
     public void setSide(int side) {
