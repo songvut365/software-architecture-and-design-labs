a29f3a3d0bd81da8e9c9b5aeee486cee0fe12d89
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-13 02:56:22 +0700

    Edit methods reset, append, getMetadataString

--- src/main/java/creational/JSONBookMetadataFormatter.java
+++ src/main/java/creational/JSONBookMetadataFormatter.java
@@ -1,22 +1,48 @@
 package creational;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+
 public class JSONBookMetadataFormatter implements BookMetadataFormatter {
+
+    private JSONObject jsonObject;
+    private JSONArray jsonArray;
+
+    public JSONBookMetadataFormatter() throws IOException {
+        reset();
+    }
 
     @Override
     public BookMetadataFormatter reset() {
-        // Please implement this method. You may create additional methods as you see fit.
-        return null;
+        jsonObject = new JSONObject();
+        jsonArray = new JSONArray();
+        return this;
     }
 
     @Override
     public BookMetadataFormatter append(Book b) {
-        // Please implement this method. You may create additional methods as you see fit.
+        JSONObject book = new JSONObject();
+
+        book.put("ISBN", b.getISBN());
+
+        JSONArray authors = new JSONArray();
+        for (String author : b.getAuthors()) {
+            authors.add(author);
+        }
+        book.put("Authors", authors);
+
+        book.put("Title", b.getTitle());
+        book.put("Publisher", b.getPublisher());
+
+        jsonArray.add(book);
+        jsonObject.put("Books", jsonArray);
         return this;
     }
 
     @Override
     public String getMetadataString() {
-        // Please implement this method. You may create additional methods as you see fit.
-        return null;
+        return jsonObject.toJSONString()+"\n";
     }
 }
