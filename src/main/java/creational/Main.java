5232c6a8bed55140cad78e707eb86dfbb634ae58
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-13 03:03:18 +0700

    Use BookMetadataExporter instead BookMeatadataFormatterFactory

--- src/main/java/creational/Main.java
+++ src/main/java/creational/Main.java
@@ -5,25 +5,39 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException {
 
         // Current usage
-        BookMetadataFormatter formatter = null;
-        try {
-            formatter = BookMetadataFormatterFactory.getBookMetadataFormatter(BookMetadataFormatterFactory.Format.CSV);
-            formatter.append(TestData.dragonBook);
-            formatter.append(TestData.dinosaurBook);
-            System.out.print(formatter.getMetadataString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
+//        BookMetadataFormatter formatter = null;
+//        try {
+//            formatter = BookMetadataFormatterFactory.getBookMetadataFormatter(BookMetadataFormatterFactory.Format.CSV);
+//            formatter.append(TestData.dragonBook);
+//            formatter.append(TestData.dinosaurBook);
+//            System.out.print(formatter.getMetadataString());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParserConfigurationException e) {
+//            e.printStackTrace();
+//        }
 
-        // Expected usage
-//        BookMetadataExporter exporter = new XMLBookMetadataExporter();
-//        exporter.add(TestData.sailboatBook);
-//        exporter.add(TestData.GoFBook);
-//        exporter.export(System.out);
+        //XML usage
+        BookMetadataExporter exporter = new XMLBookMetadataExporter();
+        exporter.add(TestData.sailboatBook);
+        exporter.add(TestData.GoFBook);
+        exporter.export(System.out);
+
+        //JSON usage
+        BookMetadataExporter jsonExporter = new JSONBookMetadataExporter();
+        jsonExporter.add(TestData.sailboatBook);
+        jsonExporter.add(TestData.sailboatBook);
+        jsonExporter.add(TestData.GoFBook);
+        jsonExporter.export(System.out);
+
+        //CSV usage
+        BookMetadataExporter csvExporter = new CSVBookMetadataExporter();
+        csvExporter.add(TestData.sailboatBook);
+        csvExporter.add(TestData.sailboatBook);
+        csvExporter.add(TestData.GoFBook);
+        csvExporter.export(System.out);
     }
 }
