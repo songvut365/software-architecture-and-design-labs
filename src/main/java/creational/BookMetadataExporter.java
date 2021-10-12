31512133f8e72b3dfdaecf6ff88992490edc7a70
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-13 03:00:19 +0700

    Add subclass XMLExporter, JSONExporter, CSVExporter

--- src/main/java/creational/BookMetadataExporter.java
+++ src/main/java/creational/BookMetadataExporter.java
@@ -1,10 +1,64 @@
 package creational;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public abstract class BookMetadataExporter extends BookCollection {
+    public abstract void export(PrintStream stream);
+    public abstract void add(Book b);
+}
 
+//XML
+class XMLBookMetadataExporter extends BookMetadataExporter {
+    BookMetadataFormatter bookMetadataFormatter = new XMLBookMetadataFormatter();
+
+    XMLBookMetadataExporter() throws ParserConfigurationException {
+    }
+
+    @Override
     public void export(PrintStream stream) {
-        // Please implement this method. You may create additional methods as you see fit.
+        stream.println(bookMetadataFormatter.getMetadataString());
+    }
+
+    @Override
+    public void add(Book b) {
+        bookMetadataFormatter.append(b);
+    }
+}
+
+//CSV
+class CSVBookMetadataExporter extends BookMetadataExporter {
+    BookMetadataFormatter bookMetadataFormatter = new CSVBookMetadataFormatter();
+
+    CSVBookMetadataExporter() throws IOException {
+    }
+
+    @Override
+    public void export(PrintStream stream) {
+        stream.println(bookMetadataFormatter.getMetadataString());
+    }
+
+    @Override
+    public void add(Book b) {
+        bookMetadataFormatter.append(b);
+    }
+}
+
+//JSON
+class JSONBookMetadataExporter extends BookMetadataExporter {
+    BookMetadataFormatter bookMetadataFormatter = new JSONBookMetadataFormatter();
+
+    JSONBookMetadataExporter() throws IOException {
+    }
+
+    @Override
+    public void export(PrintStream stream) {
+        stream.println(bookMetadataFormatter.getMetadataString());
+    }
+
+    @Override
+    public void add(Book b) {
+        bookMetadataFormatter.append(b);
     }
 }
