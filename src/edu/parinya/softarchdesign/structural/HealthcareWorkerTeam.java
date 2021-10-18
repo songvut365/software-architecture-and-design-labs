8fe28f1501f1a0c9ade6e882964d6d344dea1d38
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-19 02:19:08 +0700

    Implement composite design pattern

---
+++ src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -0,0 +1,36 @@
+package edu.parinya.softarchdesign.structural;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private final Set<HealthcareServiceable> members;
+
+    HealthcareWorkerTeam() {
+        members = new HashSet<>();
+    }
+
+    public void addMember(HealthcareServiceable worker) {
+        members.add(worker);
+    }
+
+    public void removeMember(HealthcareServiceable worker) {
+        members.remove(worker);
+    }
+
+    public void service() {
+        for(HealthcareServiceable member : members) {
+            member.service();
+        }
+    }
+
+    public double getPrice() {
+        double total = 0;
+        for(HealthcareServiceable member : members) {
+            total += member.getPrice();
+        }
+
+        return total;
+    }
+
+}
