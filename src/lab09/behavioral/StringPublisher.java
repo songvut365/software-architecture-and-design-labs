4655ef51d5c2f1016e03b3458fb47585a611eee8
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-30 13:34:19 +0700

    Implement publisher

--- src/lab09/behavioral/StringPublisher.java
+++ src/lab09/behavioral/StringPublisher.java
@@ -1,6 +1,23 @@
 package lab09.behavioral;
 
-import java.util.concurrent.Flow.Publisher;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
 
-public class StringPublisher {
+public class StringPublisher implements Flow.Publisher {
+    List<StringSubscription> subscriptions = new ArrayList<>();
+
+    @Override
+    public void subscribe(Flow.Subscriber subscriber) {
+        StringSubscription subscription = new StringSubscription(subscriber);
+        subscriptions.add(subscription);
+        subscriber.onSubscribe(subscription);
+    }
+
+    public void publishString(String str) {
+        for(StringSubscription subscription : subscriptions) {
+            subscription.publishItem(str);
+        }
+    }
 }
