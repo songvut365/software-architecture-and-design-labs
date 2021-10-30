3ef1a9755c2cb982c6bcc08695142c05fb01c26e
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-30 13:38:20 +0700

    Run observer with publish by input mode

--- src/lab09/behavioral/Main.java
+++ src/lab09/behavioral/Main.java
@@ -1,15 +1,30 @@
 package lab09.behavioral;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Publisher
+        StringPublisher publisher = new StringPublisher();
 
-        //Alphabet
+        //Subscriber
+        StringSubscriber alphabetSubscriber = new AlphabetSubscriber();
+        StringSubscriber numberSubscriber = new NumberSubscriber();
+        StringSubscriber symbolSubscriber = new SymbolSubscriber();
 
+        //Publisher subscribe to subscriber
+        publisher.subscribe(alphabetSubscriber);
+        publisher.subscribe(numberSubscriber);
+        publisher.subscribe(symbolSubscriber);
 
-        //Number
-
-
-        //Symbol
+        //Publish string
+        while(true) {
+            System.out.println("=====================");
+            System.out.print("Enter string: ");
+            Scanner input = new Scanner(System.in);
+            String myString = input.nextLine();
+            publisher.publishString(myString);
+        }
     }
 }
