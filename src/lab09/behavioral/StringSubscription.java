3c2f243e68e875d5ccc0056e7763ad4552bfe89a
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-30 13:35:06 +0700

    Implement subscription for link publisher to subscriber

--- src/lab09/behavioral/StringSubscription.java
+++ src/lab09/behavioral/StringSubscription.java
@@ -1,6 +1,36 @@
 package lab09.behavioral;
 
-import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.Flow;
 
-public class StringSubscription {
+public class StringSubscription implements Flow.Subscription {
+    Flow.Subscriber subscriber;
+    String item;
+    long counter;
+
+    public StringSubscription(Flow.Subscriber subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void request(long n) {
+        if (n < 0) {
+            this.subscriber.onError(new IllegalStateException());
+        }
+        else {
+            this.counter = n;
+        }
+    }
+
+    @Override
+    public void cancel() {
+        System.out.println("Cancel");
+    }
+
+    public void publishItem(String str) {
+        this.item = str;
+        for(int i=0; i<counter; i++) {
+            subscriber.onNext(item);
+        }
+        subscriber.onComplete();
+    }
 }
