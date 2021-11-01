8f6d6aa798e386203620c62bfa52b2788ec06945
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-01 14:08:52 +0700

    Implement sorting algorithms with Comparator

--- src/lab10/behavioral/AgeSortStrategy.java
+++ src/lab10/behavioral/AgeSortStrategy.java
@@ -1,9 +1,12 @@
-package edu.parinya.softarchdesign.behavior2;
+package lab10.behavioral;
 
 import java.util.Comparator;
 import java.util.List;
 
 public class AgeSortStrategy implements SortStrategy {
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
-
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparingInt(Person::getAge));
+    }
 }
