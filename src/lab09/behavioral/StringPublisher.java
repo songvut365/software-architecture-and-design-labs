cb14ec315921ea8b65b2da88ba3d6790cf5fd05f
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-01 10:42:29 +0700

    Update check subscriber type when publish item

--- src/lab09/behavioral/StringPublisher.java
+++ src/lab09/behavioral/StringPublisher.java
@@ -1,12 +1,14 @@
 package lab09.behavioral;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringPublisher implements Flow.Publisher {
     List<StringSubscription> subscriptions = new ArrayList<>();
+    private String StringBuffer;
 
     @Override
     public void subscribe(Flow.Subscriber subscriber) {
@@ -15,9 +17,30 @@ public class StringPublisher implements Flow.Publisher {
         subscriber.onSubscribe(subscription);
     }
 
+    //Publish String
     public void publishString(String str) {
+        StringBuffer = str;
+
+        //loop all subscription
         for(StringSubscription subscription : subscriptions) {
-            subscription.publishItem(str);
+
+            //check subscriber type
+            if(subscription.subscriber instanceof AlphabetSubscriber && hasPattern("[a-zA-z]")) {
+                subscription.publishItem(StringBuffer);
+            }
+            if(subscription.subscriber instanceof NumberSubscriber && hasPattern("[0-9]")) {
+                subscription.publishItem(StringBuffer);
+            }
+            if(subscription.subscriber instanceof SymbolSubscriber && hasPattern("[,!@#$%&*()_+=|<>?{}\\\\[\\\\]~-]")) {
+                subscription.publishItem(StringBuffer);
+            }
         }
     }
+
+    private boolean hasPattern( String pattern) {
+        Pattern stringItem = Pattern.compile(pattern);
+        Matcher result = stringItem.matcher(StringBuffer);
+        return result.find();
+    }
+
 }
